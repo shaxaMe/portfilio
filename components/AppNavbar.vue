5a43d15847d6c4477176302495cc4448ff803835
shaxaMe <ismatovshahzod43@gmail.com>
2024-03-13 17:39:28 +0500

    add prod

---
+++ components/AppNavbar.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="w-full bg-transparent flex items-center justify-between nav-content gap-3 text-white px-8 py-4 mt-8 rounded-full shadow-lg">
+      <span class="font-extrabold cursor-pointer text-4xl flex-1">Bek Ismatov</span>
+      <ul class="flex justify-between flex-1">
+       <li class="text-base rounded-lg hover:border-b-[#F237F6] border-b-[3px] transition-all border-transparent  cursor-pointer font-medium">HOME</li>   
+       <li class="text-base rounded-lg hover:border-b-[#F237F6] border-b-[3px] transition-all border-transparent  cursor-pointer font-medium">PROJECTS </li>
+       <li class="text-base rounded-lg hover:border-b-[#F237F6] border-b-[3px] transition-all border-transparent  cursor-pointer font-medium">ABOUT</li>
+       <li class="text-base rounded-lg hover:border-b-[#F237F6] border-b-[3px] transition-all border-transparent  cursor-pointer font-medium">CONTACT</li>
+      </ul>
+      <!-- <div class="flex-1">
+        LIGHT
+        DARK
+      </div> -->
+      <div class="flex-1 flex justify-end">
+        <button class="w-fit bg-[#242121] rounded-full flex justify-center items-center text-xl font-semibold text-white py-2 px-5">
+        Contact Me
+      </button>
+      </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style lang="scss">
+.nav-content{
+    background-image: linear-gradient(to right, #340544, #320333, #2c0424, #240419, #1b020e, #1e0311, #210414, #230517, #310529, #3a0442, #360e61, #0a1f85);
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,22 @@
+<template>
+    <section class="main-page">
+        <div class="container mx-auto my-0">
+           <AppNavbar />
+        </div>
+    </section>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss">
+.main-page{
+    min-width: 100dvw;
+    min-height: 100dvh;
+    background-image: url('~/assets/bg/bg-1.png');
+    background-size: cover;
+    // background-position: center;
+    background-repeat: no-repeat;
+}
+</style>
